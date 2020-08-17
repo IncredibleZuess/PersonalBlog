f3108770a32018843a1959553dc02f2f95c25bf9
FirstGitTesting <carlo.barnardo@gmail.com>
2020-08-18 01:25:40 +0200

    Styling Changes

--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,6 @@ export default function Home({allPostsData}) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi! I am IncredibleZuess a 18 year old developer aspiring to be one of the best programmers out there!</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
